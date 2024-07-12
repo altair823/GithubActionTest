5438d5e03fbf05656b8403e1c90ab1b97016f995
altair823 <kimth0734@gmail.com>
2024-07-12 16:42:13 +0900

    fix github action

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+PROJECT_ROOT="/home/ubuntu/app"
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+JAR_FILE="$PROJECT_ROOT/github-actions-demo.jar"
+
 # Run the server
 cd /home/ubuntu/app || exit
 
-java -jar *.jar
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
